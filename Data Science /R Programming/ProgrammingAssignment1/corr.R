d5e257a1c41f91a3392f548ef7096510346459ee
Cheongsoo <Cheongsoo@users.noreply.github.com>
2018-07-04 16:51:18 -0700

    Update corr.R

--- Data Science /R Programming/ProgrammingAssignment1/corr.R
+++ Data Science /R Programming/ProgrammingAssignment1/corr.R
@@ -12,11 +12,11 @@ corr <- function(directory, threshold = 0) {
         files <- list.files(directory, full.names = TRUE)
         correlation <- NULL
         for(i in seq_along(files)) {
-                data <- read.csv(files[i])
-                complete <- complete.cases(data)
+                read_data <- read.csv(files[i])
+                complete <- complete.cases(read_data)
                 
                 if(sum(complete) > threshold) {
-                        data <- data.frame(data[which(complete),]['sulfate'], data[which(complete),]['nitrate'])
+                        data <- data.frame(read_data[which(complete),]['sulfate'], read_data[which(complete),]['nitrate'])
                         corr_data <- cor(data$sulfate, data$nitrate)
                         correlation <- c(correlation, corr_data)
                 } 
