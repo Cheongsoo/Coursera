40989b2faa13969e2d7ecf48354420411eda5733
Cheongsoo <Cheongsoo@users.noreply.github.com>
2018-07-03 19:55:50 -0700

    Update complete.R

--- Data Science /R Programming/ProgrammingAssignment1/complete.R
+++ Data Science /R Programming/ProgrammingAssignment1/complete.R
@@ -1,3 +1,12 @@
+#Write a function that reads a directory full of files and 
+#reports the number of completely observed cases in each data file. 
+#The function should return a data frame where the first column is the name of the file 
+#and the second column is the number of complete cases. 
+
+
+
+
+
 complete <- function(directory, id = 1:322) {
         files_list <- list.files(directory, full.names = TRUE)
         z <- length(id)
